c8206edbed463f4b4d667134839c480ae3e86ca8
kimziou77 <kimziou77@naver.com>
2021-12-27 13:17:14 +0900

    Feat: Singleton Pattern

---
+++ src/Singleton/BClazz.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class BClazz {
+    private SocketClient socketClient;
+    public BClazz(){
+        this.socketClient = SocketClient.getInstance();
+    }
+    public SocketClient getSocketClient(){
+        return this.socketClient;
+    }
+}
